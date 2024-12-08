7ea70646906c604b54ec19f817b61a3b981f2ced
Tom Hackshaw <gh@tomhackshaw.com>
2024-12-08 19:19:45 +1300

    feat: Add live counter to Documento home page

--- apps/(tools)/documento/app/(home)/layout.tsx
+++ apps/(tools)/documento/app/(home)/layout.tsx
@@ -1,7 +1,13 @@
 import { baseOptions } from "@/app/layout.config";
+import Live from "@/components/live";
 import { HomeLayout } from "fumadocs-ui/layouts/home";
 import type { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
+  return (
+    <HomeLayout {...baseOptions}>
+      <Live />
+      {children}
+    </HomeLayout>
+  );
 }
